ba3a21edf0c0924214868ce483fc439a42470c2b
Catversal <mmoga.my@gmail.com>
2026-01-05 13:47:53 +0100

    Add files via upload

---
+++ Serverpack-Creator/config/system-files/windows-files/Remove-Lists.bat
@@ -0,0 +1,16 @@
+echo off >nul
+REM List to Set Removal list based on Modpack
+
+
+set "REMOVE_LIST="
+
+Rem Beyond Asension
+if "%MODPACK%"=="1" set "REMOVE_LIST=%LISTS%\beyond_ascension_remove.txt"
+
+REM Beyond Cosmo
+if "%MODPACK%"=="2" set "REMOVE_LIST=%LISTS%\beyond_cosmos_remove.txt"
+
+REM Beyond Depth
+if "%MODPACK%"=="3" set "REMOVE_LIST=%LISTS%\beyond_depth_remove.txt"
+
+exit /b
---
+++ Serverpack-Creator/config/system-files/windows-files/Server-Icon.bat
@@ -0,0 +1,21 @@
+echo off >nul
+REM Script to Grab Server Icon from Curseforge Page
+
+REM Beyond Ascension
+if "%MODPACK%"=="1" (
+    set "ICON_URL=https://media.forgecdn.net/avatars/thumbnails/1311/957/64/64/638853138308838053_animated.gif"
+)
+
+REM Beyond Cosmo
+if "%MODPACK%"=="2" (
+    set "ICON_URL=https://media.forgecdn.net/avatars/thumbnails/1319/80/64/64/638857668194537957_animated.gif"
+)
+
+REM Beyond Depth
+if "%MODPACK%"=="3" (
+    set "ICON_URL=https://media.forgecdn.net/avatars/thumbnails/1311/962/64/64/638853142230249135_animated.gif"
+)
+powershell -Command ^
+    "Invoke-WebRequest '%ICON_URL%' -OutFile '%SERVERPACK%\server-icon.png'"
+
+exit /b
